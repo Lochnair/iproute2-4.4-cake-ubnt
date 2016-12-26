7f262d9d5a04ce727cff7b339c2c524853f924c2
Nils Andreas Svee <me@lochnair.net>
2016-12-26 22:24:27 +0100

    Forgot to add q_fq_pie.c from Hironori's repo

--- tc/q_fq_pie.c
+++ tc/q_fq_pie.c
@@ -1,7 +1,7 @@
 /*
- * Fair Queue Pie
+ * Fair Queue PIE
  *
- *  Copyright (C) 2014 Hironori Okano <hokano@cisco.com>
+ *  Copyright (C) 2015 Hironori Okano <hokano@cisco.com>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -44,74 +44,57 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <string.h>
-#include <stdbool.h>
 
 #include "utils.h"
 #include "tc_util.h"
+
+static void explain(void)
+{
+	fprintf(stderr, "Usage: ... fq_pie [ limit PACKETS ] [ flows NUMBER ]\n");
+	fprintf(stderr, "                  [ target TIME us] [ tupdate TUPDATE ]\n");
+	fprintf(stderr, "                  [ quantum BYTES ] [ [no]ecn ]\n");
+	fprintf(stderr, "                  [ alpha ALPHA ] [beta BETA ]\n");
+	fprintf(stderr, "                  [bytemode | nobytemode]\n");
+}
 
 #define ALPHA_MAX 32
 #define ALPHA_MIN 0
 #define BETA_MAX 32
 #define BETA_MIN 0
 
-static void explain(void)
-{
-	fprintf(stderr, "Usage: ... pie [ limit PACKETS ][ target TIME us]\n");
-	fprintf(stderr, "              [ tupdate TIME us][ alpha ALPHA ]");
-	fprintf(stderr, "[beta BETA ][bytemode | nobytemode][ecn | noecn ]\n");
-
-	fprintf(stderr, "Usage: ... fq [ limit PACKETS ] [ flow_limit PACKETS ]\n");
-	fprintf(stderr, "              [ quantum BYTES ] [ initial_quantum BYTES ]\n");
-	fprintf(stderr, "              [ maxrate RATE  ] [ buckets NUMBER ]\n");
-	fprintf(stderr, "              [ [no]pacing ]\n");
-}
-
-static unsigned int ilog2(unsigned int val)
-{
-	unsigned int res = 0;
-
-	val--;
-	while (val) {
-		res++;
-		val >>= 1;
-	}
-	return res;
-}
-
 static int fq_pie_parse_opt(struct qdisc_util *qu, int argc, char **argv,
-			struct nlmsghdr *n)
+			      struct nlmsghdr *n)
 {
-	unsigned int limit   = 0;
-	unsigned int target  = 0;
-	unsigned int tupdate = 0;
-	unsigned int alpha   = 0;
-	unsigned int beta    = 0;
+	unsigned limit = 0;
+	unsigned flows = 0;
+	unsigned target = 0;
+	unsigned quantum = 0;
+	unsigned tupdate = 0;
+	unsigned alpha   = 0;
+	unsigned beta    = 0;
 	int ecn = -1;
 	int bytemode = -1;
-	unsigned int plimit;
-	unsigned int flow_plimit;
-	unsigned int quantum;
-	unsigned int initial_quantum;
-	unsigned int buckets = 0;
-	unsigned int maxrate;
-	unsigned int defrate;
-	bool set_plimit = false;
-	bool set_flow_plimit = false;
-	bool set_quantum = false;
-	bool set_initial_quantum = false;
-	bool set_maxrate = false;
-	bool set_defrate = false;
-	int pacing = -1;
 	struct rtattr *tail;
 
 	while (argc > 0) {
 		if (strcmp(*argv, "limit") == 0) {
 			NEXT_ARG();
-			if (get_unsigned(&plimit, *argv, 0)) {
+			if (get_unsigned(&limit, *argv, 0)) {
 				fprintf(stderr, "Illegal \"limit\"\n");
 				return -1;
 			}
-			set_plimit = true;
+		} else if (strcmp(*argv, "flows") == 0) {
+			NEXT_ARG();
+			if (get_unsigned(&flows, *argv, 0)) {
+				fprintf(stderr, "Illegal \"flows\"\n");
+				return -1;
+			}
+		} else if (strcmp(*argv, "quantum") == 0) {
+			NEXT_ARG();
+			if (get_unsigned(&quantum, *argv, 0)) {
+				fprintf(stderr, "Illegal \"quantum\"\n");
+				return -1;
+			}
 		} else if (strcmp(*argv, "target") == 0) {
 			NEXT_ARG();
 			if (get_time(&target, *argv)) {
@@ -146,51 +129,6 @@ static int fq_pie_parse_opt(struct qdisc_util *qu, int argc, char **argv,
 			bytemode = 1;
 		} else if (strcmp(*argv, "nobytemode") == 0) {
 			bytemode = 0;
-		} else if (strcmp(*argv, "flow_limit") == 0) {
-			NEXT_ARG();
-			if (get_unsigned(&flow_plimit, *argv, 0)) {
-				fprintf(stderr, "Illegal \"flow_limit\"\n");
-				return -1;
-			}
-			set_flow_plimit = true;
-		} else if (strcmp(*argv, "buckets") == 0) {
-			NEXT_ARG();
-			if (get_unsigned(&buckets, *argv, 0)) {
-				fprintf(stderr, "Illegal \"buckets\"\n");
-				return -1;
-			}
-		} else if (strcmp(*argv, "maxrate") == 0) {
-			NEXT_ARG();
-			if (get_rate(&maxrate, *argv)) {
-				fprintf(stderr, "Illegal \"maxrate\"\n");
-				return -1;
-			}
-			set_maxrate = true;
-		} else if (strcmp(*argv, "defrate") == 0) {
-			NEXT_ARG();
-			if (get_rate(&defrate, *argv)) {
-				fprintf(stderr, "Illegal \"defrate\"\n");
-				return -1;
-			}
-			set_defrate = true;
-		} else if (strcmp(*argv, "quantum") == 0) {
-			NEXT_ARG();
-			if (get_unsigned(&quantum, *argv, 0)) {
-				fprintf(stderr, "Illegal \"quantum\"\n");
-				return -1;
-			}
-			set_quantum = true;
-		} else if (strcmp(*argv, "initial_quantum") == 0) {
-			NEXT_ARG();
-			if (get_unsigned(&initial_quantum, *argv, 0)) {
-				fprintf(stderr, "Illegal \"initial_quantum\"\n");
-				return -1;
-			}
-			set_initial_quantum = true;
-		} else if (strcmp(*argv, "pacing") == 0) {
-			pacing = 1;
-		} else if (strcmp(*argv, "nopacing") == 0) {
-			pacing = 0;
 		} else if (strcmp(*argv, "help") == 0) {
 			explain();
 			return -1;
@@ -199,21 +137,22 @@ static int fq_pie_parse_opt(struct qdisc_util *qu, int argc, char **argv,
 			explain();
 			return -1;
 		}
-		argc--; argv++;
+		argc--;
+		argv++;
 	}
 
 	tail = NLMSG_TAIL(n);
 	addattr_l(n, 1024, TCA_OPTIONS, NULL, 0);
-	if (buckets) {
-		unsigned int log = ilog2(buckets);
-
-		addattr_l(n, 1024, TCA_FQ_PIE_BUCKETS_LOG,
-			  &log, sizeof(log));
-	}
-	if (target)
-		addattr_l(n, 1024, TCA_FQ_PIE_TARGET, &target, sizeof(target));
+	if (limit)
+		addattr_l(n, 1024, TCA_FQ_PIE_LIMIT, &limit, sizeof(limit));
+	if (flows)
+		addattr_l(n, 1024, TCA_FQ_PIE_FLOWS, &flows, sizeof(flows));
+	if (quantum)
+		addattr_l(n, 1024, TCA_FQ_PIE_QUANTUM, &quantum, sizeof(quantum));
 	if (tupdate)
 		addattr_l(n, 1024, TCA_FQ_PIE_TUPDATE, &tupdate, sizeof(tupdate));
+	if (target)
+		addattr_l(n, 1024, TCA_FQ_PIE_TARGET, &target, sizeof(target));
 	if (alpha)
 		addattr_l(n, 1024, TCA_FQ_PIE_ALPHA, &alpha, sizeof(alpha));
 	if (beta)
@@ -223,26 +162,6 @@ static int fq_pie_parse_opt(struct qdisc_util *qu, int argc, char **argv,
 	if (bytemode != -1)
 		addattr_l(n, 1024, TCA_FQ_PIE_BYTEMODE, &bytemode,
 			  sizeof(bytemode));
-	if (set_plimit)
-		addattr_l(n, 1024, TCA_FQ_PIE_PLIMIT,
-			  &plimit, sizeof(plimit));
-	if (set_flow_plimit)
-		addattr_l(n, 1024, TCA_FQ_PIE_FLOW_PLIMIT,
-			  &flow_plimit, sizeof(flow_plimit));
-	if (set_quantum)
-		addattr_l(n, 1024, TCA_FQ_PIE_QUANTUM, &quantum, sizeof(quantum));
-	if (set_initial_quantum)
-		addattr_l(n, 1024, TCA_FQ_PIE_INITIAL_QUANTUM,
-			  &initial_quantum, sizeof(initial_quantum));
-	if (pacing != -1)
-		addattr_l(n, 1024, TCA_FQ_PIE_RATE_ENABLE,
-			  &pacing, sizeof(pacing));
-	if (set_maxrate)
-		addattr_l(n, 1024, TCA_FQ_PIE_FLOW_MAX_RATE,
-			  &maxrate, sizeof(maxrate));
-	if (set_defrate)
-		addattr_l(n, 1024, TCA_FQ_PIE_FLOW_DEFAULT_RATE,
-			  &defrate, sizeof(defrate));
 	tail->rta_len = (void *) NLMSG_TAIL(n) - (void *) tail;
 	return 0;
 }
@@ -250,22 +169,37 @@ static int fq_pie_parse_opt(struct qdisc_util *qu, int argc, char **argv,
 static int fq_pie_print_opt(struct qdisc_util *qu, FILE *f, struct rtattr *opt)
 {
 	struct rtattr *tb[TCA_FQ_PIE_MAX + 1];
-	unsigned int target;
-	unsigned int tupdate;
-	unsigned int alpha;
-	unsigned int beta;
+	unsigned limit;
+	unsigned flows;
+	unsigned tupdate;
+	unsigned target;
+	unsigned alpha;
+	unsigned beta;
 	unsigned ecn;
+	unsigned quantum;
 	unsigned bytemode;
-	unsigned int plimit, flow_plimit;
-	unsigned int buckets_log;
-	int pacing;
-	unsigned int rate, quantum;
 	SPRINT_BUF(b1);
 
 	if (opt == NULL)
 		return 0;
 
 	parse_rtattr_nested(tb, TCA_FQ_PIE_MAX, opt);
+
+	if (tb[TCA_FQ_PIE_LIMIT] &&
+	    RTA_PAYLOAD(tb[TCA_FQ_PIE_LIMIT]) >= sizeof(__u32)) {
+		limit = rta_getattr_u32(tb[TCA_FQ_PIE_LIMIT]);
+		fprintf(f, "limit %up ", limit);
+	}
+	if (tb[TCA_FQ_PIE_FLOWS] &&
+	    RTA_PAYLOAD(tb[TCA_FQ_PIE_FLOWS]) >= sizeof(__u32)) {
+		flows = rta_getattr_u32(tb[TCA_FQ_PIE_FLOWS]);
+		fprintf(f, "flows %u ", flows);
+	}
+	if (tb[TCA_FQ_PIE_QUANTUM] &&
+	    RTA_PAYLOAD(tb[TCA_FQ_PIE_QUANTUM]) >= sizeof(__u32)) {
+		quantum = rta_getattr_u32(tb[TCA_FQ_PIE_QUANTUM]);
+		fprintf(f, "quantum %u ", quantum);
+	}
 	if (tb[TCA_FQ_PIE_TARGET] &&
 	    RTA_PAYLOAD(tb[TCA_FQ_PIE_TARGET]) >= sizeof(__u32)) {
 		target = rta_getattr_u32(tb[TCA_FQ_PIE_TARGET]);
@@ -286,72 +220,26 @@ static int fq_pie_print_opt(struct qdisc_util *qu, FILE *f, struct rtattr *opt)
 		beta = rta_getattr_u32(tb[TCA_FQ_PIE_BETA]);
 		fprintf(f, "beta %u ", beta);
 	}
-
-	if (tb[TCA_FQ_PIE_ECN] && RTA_PAYLOAD(tb[TCA_FQ_PIE_ECN]) >= sizeof(__u32)) {
+	if (tb[TCA_FQ_PIE_ECN] &&
+	    RTA_PAYLOAD(tb[TCA_FQ_PIE_ECN]) >= sizeof(__u32)) {
 		ecn = rta_getattr_u32(tb[TCA_FQ_PIE_ECN]);
 		if (ecn)
 			fprintf(f, "ecn ");
 	}
-
 	if (tb[TCA_FQ_PIE_BYTEMODE] &&
 	    RTA_PAYLOAD(tb[TCA_FQ_PIE_BYTEMODE]) >= sizeof(__u32)) {
 		bytemode = rta_getattr_u32(tb[TCA_FQ_PIE_BYTEMODE]);
 		if (bytemode)
 			fprintf(f, "bytemode ");
 	}
-	if (tb[TCA_FQ_PIE_PLIMIT] &&
-	    RTA_PAYLOAD(tb[TCA_FQ_PIE_PLIMIT]) >= sizeof(__u32)) {
-		plimit = rta_getattr_u32(tb[TCA_FQ_PIE_PLIMIT]);
-		fprintf(f, "limit %up ", plimit);
-	}
-	if (tb[TCA_FQ_PIE_FLOW_PLIMIT] &&
-	    RTA_PAYLOAD(tb[TCA_FQ_PIE_FLOW_PLIMIT]) >= sizeof(__u32)) {
-		flow_plimit = rta_getattr_u32(tb[TCA_FQ_PIE_FLOW_PLIMIT]);
-		fprintf(f, "flow_limit %up ", flow_plimit);
-	}
-	if (tb[TCA_FQ_PIE_BUCKETS_LOG] &&
-	    RTA_PAYLOAD(tb[TCA_FQ_PIE_BUCKETS_LOG]) >= sizeof(__u32)) {
-		buckets_log = rta_getattr_u32(tb[TCA_FQ_PIE_BUCKETS_LOG]);
-		fprintf(f, "buckets %u ", 1U << buckets_log);
-	}
-	if (tb[TCA_FQ_PIE_RATE_ENABLE] &&
-	    RTA_PAYLOAD(tb[TCA_FQ_PIE_RATE_ENABLE]) >= sizeof(int)) {
-		pacing = rta_getattr_u32(tb[TCA_FQ_PIE_RATE_ENABLE]);
-		if (pacing == 0)
-			fprintf(f, "nopacing ");
-	}
-	if (tb[TCA_FQ_PIE_QUANTUM] &&
-	    RTA_PAYLOAD(tb[TCA_FQ_PIE_QUANTUM]) >= sizeof(__u32)) {
-		quantum = rta_getattr_u32(tb[TCA_FQ_PIE_QUANTUM]);
-		fprintf(f, "quantum %u ", quantum);
-	}
-	if (tb[TCA_FQ_PIE_INITIAL_QUANTUM] &&
-	    RTA_PAYLOAD(tb[TCA_FQ_PIE_INITIAL_QUANTUM]) >= sizeof(__u32)) {
-		quantum = rta_getattr_u32(tb[TCA_FQ_PIE_INITIAL_QUANTUM]);
-		fprintf(f, "initial_quantum %u ", quantum);
-	}
-	if (tb[TCA_FQ_PIE_FLOW_MAX_RATE] &&
-	    RTA_PAYLOAD(tb[TCA_FQ_PIE_FLOW_MAX_RATE]) >= sizeof(__u32)) {
-		rate = rta_getattr_u32(tb[TCA_FQ_PIE_FLOW_MAX_RATE]);
-
-		if (rate != ~0U)
-			fprintf(f, "maxrate %s ", sprint_rate(rate, b1));
-	}
-	if (tb[TCA_FQ_PIE_FLOW_DEFAULT_RATE] &&
-	    RTA_PAYLOAD(tb[TCA_FQ_PIE_FLOW_DEFAULT_RATE]) >= sizeof(__u32)) {
-		rate = rta_getattr_u32(tb[TCA_FQ_PIE_FLOW_DEFAULT_RATE]);
-
-		if (rate != 0)
-			fprintf(f, "defrate %s ", sprint_rate(rate, b1));
-	}
 
 	return 0;
 }
 
 static int fq_pie_print_xstats(struct qdisc_util *qu, FILE *f,
-			   struct rtattr *xstats)
+				 struct rtattr *xstats)
 {
-	struct tc_fq_pie_qd_stats *st;
+	struct tc_fq_pie_xstats *st;
 
 	if (xstats == NULL)
 		return 0;
@@ -360,33 +248,22 @@ static int fq_pie_print_xstats(struct qdisc_util *qu, FILE *f,
 		return -1;
 
 	st = RTA_DATA(xstats);
-	fprintf(f, "prob %f delay %uus avg_dq_rate %u",
-		(double)st->prob / (double)0xffffffff, st->delay,
-		st->avg_dq_rate);
-	fprintf(f, "pkts_in %u overlimit %u dropped %u maxq %u ecn_mark %u",
-		st->packets_in, st->overlimit, st->dropped, st->maxq,
-		st->ecn_mark);
-	fprintf(f, "  %u flows (%u inactive, %u throttled)",
-		st->flows, st->inactive_flows, st->throttled_flows);
-
-	if (st->time_next_delayed_flow > 0)
-		fprintf(f, ", next packet delay %llu ns", st->time_next_delayed_flow);
-
-	fprintf(f, "\n  %llu gc, %llu highprio",
-		st->gc_flows, st->highprio_packets);
-
-	if (st->tcp_retrans)
-		fprintf(f, ", %llu retrans", st->tcp_retrans);
-
-	fprintf(f, ", %llu throttled", st->throttled);
-
-	if (st->flows_plimit)
-		fprintf(f, ", %llu flows_plimit", st->flows_plimit);
-
-	if (st->pkts_too_long || st->allocation_errors)
-		fprintf(f, "\n  %llu too long pkts, %llu alloc errors\n",
-			st->pkts_too_long, st->allocation_errors);
-
+	if (st->type == TCA_FQ_PIE_XSTATS_QDISC) {
+		fprintf(f, "  maxq %u packets_in %u dropped %u \
+			drop_overlimit %u new_flow_count %u ecn_mark %u",
+			st->qdisc_stats.maxq,
+			st->qdisc_stats.packets_in,
+			st->qdisc_stats.dropped,
+			st->qdisc_stats.drop_overlimit,
+			st->qdisc_stats.new_flow_count,
+			st->qdisc_stats.ecn_mark);
+		fprintf(f, "\n  new_flows_len %u old_flows_len %u",
+			st->qdisc_stats.new_flows_len,
+			st->qdisc_stats.old_flows_len);
+	}
+	if (st->type == TCA_FQ_PIE_XSTATS_CLASS) {
+		fprintf(f, "  deficit %d", st->class_stats.deficit);
+	}
 	return 0;
 }
 
